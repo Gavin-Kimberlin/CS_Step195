98134b3c05fb9164df1022740a5359817814af3f
Gavin Kimberlin <guviner@gmail.com>
2020-08-04 19:11:20 -0700

    Create a program that will check if the user entered a day of the week.

--- CS_Step195/Program.cs
+++ CS_Step195/Program.cs
@@ -10,6 +10,29 @@ namespace CS_Step195
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What is current day of the week?\n");
+            string CurrentDay = Convert.ToString(Console.ReadLine()).ToLower();
+
+            try
+            {
+                daysOfTheWeek day = (daysOfTheWeek)Enum.Parse(typeof(daysOfTheWeek), CurrentDay);
+                Console.WriteLine("\nToday is " + char.ToUpper(CurrentDay[0]) + CurrentDay.Substring(1));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nPlease enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+        enum daysOfTheWeek
+        {
+            monday,
+            tuesday,
+            wednesday,
+            thursday,
+            friday,
+            saturday,
+            sunday
         }
     }
 }
